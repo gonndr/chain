44f386b3dd18c318c58f992b1ccd877bb08140e1
goncalogomes <gonndr@gmail.com>
2022-05-05 00:52:54 +0100

    add mining

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,27 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -34,7 +48,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -56,7 +70,9 @@ class Blockchain {
 }
 
 let myCoin = new Blockchain();
+console.log("mining block 1...");
 myCoin.addBlock(new Block(1, "01/02/2022", { amount: 4 }));
+console.log("mining block 2...");
 myCoin.addBlock(new Block(2, "12/02/2022", { amount: 10 }));
 
 console.log(myCoin.isChainValid());
