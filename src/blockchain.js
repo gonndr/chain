b05cc8742f195d3b3ee5aadbc62e420b22f61688
goncalogomes <gonndr@gmail.com>
2022-05-05 02:01:48 +0100

    add transaction signing

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,10 +1,37 @@
 const SHA256 = require("crypto-js/sha256");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 
 class Transaction {
   constructor(fromAddress, toAddress, amount) {
     this.fromAddress = fromAddress;
     this.toAddress = toAddress;
     this.amount = amount;
+  }
+
+  calculateHash() {
+    return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+  }
+
+  signTransaction(signingKey) {
+    if (signingKey.getPublic("hex") !== this.fromAddress) {
+      throw new Error("You can't spend transactions from other wallets!");
+    }
+
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, "base64");
+    this.signature = sig.toDER("hex");
+  }
+
+  isValid() {
+    if (this.fromAddress === null) return true;
+
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error("No signature in this transaction");
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, "hex");
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
@@ -34,6 +61,15 @@ class Block {
       this.hash = this.calculateHash();
     }
   }
+
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 class Blockchain {
@@ -54,17 +90,25 @@ class Blockchain {
 
   minePendingTransactions(miningRewardAddress) {
     let block = new Block(Date.now(), this.pendingTransactions); // miners should be able to choose which transactions are included
+    block.transactions.push(
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    );
     block.mineBlock(this.difficulty);
 
     console.log("Block successfully mined!");
     this.chain.push(block);
 
-    this.pendingTransactions = [
-      new Transaction(null, miningRewardAddress, this.miningReward),
-    ];
+    this.pendingTransactions = [];
   }
 
-  createTransaction(transaction) {
+  addTransaction(transaction) {
+    if (!transaction.fromAddress || !transaction.toAddress) {
+      throw new Error("Transaction must include from and to address");
+    }
+
+    if (!transaction.isValid()) {
+      throw new Error("Cannot add invalid transaction to chain");
+    }
     this.pendingTransactions.push(transaction);
   }
 
@@ -89,6 +133,10 @@ class Blockchain {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
+
+      if (!currentBlock.hasValidTransactions()) {
+        return false;
+      }
 
       if (currentBlock.hash !== currentBlock.calculateHash()) {
         return false;
--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 const { Blockchain, Transaction } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const myKey = ec.keyFromPrivate(
+  "0460c6ec715120de0da821216e6903bde6ed596c3604e329706d62187b30404e5ea83937907fd6a0b6ae955fe71cdac67a72c0a2a18bea8d2d954e3f0a5522e40a"
+);
+const myWalletAddress = myKey.getPublic("hex");
 
 let myCoin = new Blockchain();
-myCoin.createTransaction(new Transaction("address1", "address2", 100));
-myCoin.createTransaction(new Transaction("address2", "address1", 50));
+
+const tx1 = new Transaction(myWalletAddress, "public key", 10);
+tx1.signTransaction(myKey);
+myCoin.addTransaction(tx1);
 
 console.log("\n Starting the miner...");
-myCoin.minePendingTransactions("address3");
+myCoin.minePendingTransactions(myWalletAddress);
 
 console.log(
-  "\n Balance of address 3 is",
-  myCoin.getBalanceOfAddress("address3")
+  "\n Balance of my wallet is",
+  myCoin.getBalanceOfAddress(myWalletAddress)
 );
 
-console.log("\n Starting the miner...");
-myCoin.minePendingTransactions("address3");
-
-console.log(
-  "\n Balance of address 3 is",
-  myCoin.getBalanceOfAddress("address3")
-);
+console.log(myCoin.pendingTransactions);
